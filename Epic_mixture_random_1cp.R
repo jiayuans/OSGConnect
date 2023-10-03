a0a1db3d7fe2235f46537ab26c630a11721f4229
Jiayuan Shi <jiayuans422@gmail.com>
2023-10-02 18:29:52 -0600

    new

--- Epic_mixture_random_1cp.R
+++ Epic_mixture_random_1cp.R
@@ -9,8 +9,4 @@ library(runjags)
 library(tidyverse)
 
 dat0 <- read.csv(file="Data-PA-cohort.csv")
-set.seed(2256)
-dat<- dat0[dat0$ru < 0.2, -1] #subset of the data
-length(unique(dat0$cffidno)) #1734
-length(unique(dat$cffidno)) #323
-head(dat, n=10)
+dat0
