44127d0c62f58e218462be0d308de430ade30546
Jiayuan Shi <jiayuans422@gmail.com>
2024-06-05 09:56:56 -0600

    u

--- Qualify/Q4i.R
+++ Qualify/Q4i.R
@@ -1,3 +1,5 @@
+#!/usr/bin/env Rscript
+
 library(tidyverse)
 library(dplyr)
 library(Matrix)
@@ -131,7 +133,7 @@ simulate_and_fit <- function(num_simulations) {
   
 }
 
-num_sim <- 1
+num_sim <- 5000
 alpha <- 0.05
 FWER <- simulate_and_fit(num_sim)
 FWER
